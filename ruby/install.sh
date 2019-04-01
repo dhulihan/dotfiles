4142c91dd68833b541277914c1d910ee976307aa
Dave Hulihan <dhulihan@gmail.com>
2019-03-31 23:35:44 -0600

    update ruby

--- ruby/install.sh
+++ ruby/install.sh
@@ -2,23 +2,21 @@
 
 if test ! $(which rbenv)
 then
-  echo "  Installing rbenv for you."
+  echo "Installing rbenv for you."
   brew install rbenv > /tmp/rbenv-install.log
 fi
 
 if test ! $(which ruby-build)
 then
-  echo "  Installing ruby-build for you."
+  echo "Installing ruby-build for you."
   brew install ruby-build > /tmp/ruby-build-install.log
 fi
 
 # ruby version to install
-RUBY_VERSION=2.5.1
+RUBY_VERSION=2.6.2
 
 rbenv install $RUBY_VERSION
 rbenv global $RUBY_VERSION
-
-gem install bundler
 
 # Install utility gems
 bundle install
