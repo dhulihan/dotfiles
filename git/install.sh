75f14fbf58fe20c3284d6763d03dfcd48c82c3d3
dhulihan <dave.hulihan@getweave.com>
2019-07-23 15:27:48 -0600

    git

--- git/install.sh
+++ git/install.sh
@@ -1,2 +1,5 @@
 # Use git template directory when creating or cloning projects.
 git config --global init.templatedir '~/.git_template'
+
+# global gitignore
+git config --global core.excludesfile ~/.gitignore_global
