b9eaea480aeb92514d388dec72a1b02d10973287
dhulihan <dhulihan@gmail.com>
2019-08-14 21:15:37 -0600

    zsh vundle

--- vim/install.sh
+++ vim/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 # install plugins (currently broken, run manually)
 #vim -c ":source ~/.vimrc | PluginInstall | q"
 #vim -c ":GoInstallBinaries | q"
--- zsh/install.sh
+++ zsh/install.sh
@@ -3,3 +3,6 @@ chsh -s $(which zsh)
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+
+
