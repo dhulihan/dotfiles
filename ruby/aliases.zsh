a49557b82df64667723875a2e5791a802f97ea3b
Dave Hulihan <dave.hulihan@getweave.com>
2023-01-30 09:47:15 -0700

    use git as indicator of dev dir

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -4,4 +4,4 @@ alias sd='script/destroy'
 
 alias dbreset="bundle exec rake db:migrate VERSION=0 --trace ; bundle exec rake db:migrate --trace ; bundle exec rake db:seed --trace ; bundle exec rake db:sample --trace"
 alias dbreset_test="bundle exec rake db:migrate VERSION=0 RAILS_ENV=test --trace ; bundle exec rake db:migrate --trace RAILS_ENV=test ; bundle exec rake db:seed RAILS_ENV=test"
-alias b="bundle exec"
+#alias b="bundle exec"
