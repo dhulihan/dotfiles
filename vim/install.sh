16d03e83e2a5ace26cf31d1c080a615f25ff81ee
Dave Hulihan <dave.hulihan@getweave.com>
2023-04-07 14:31:37 -0600

    switch vundle to vim-plug

--- vim/install.sh
+++ vim/install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # install plugins (currently broken, run manually)
 #vim -c ":source ~/.vimrc | PluginInstall | q"
