56ba0a96af5670955397daa6fffe6c3c984deb19
Dave Hulihan <dave.hulihan@getweave.com>
2024-04-01 13:56:30 -0600

    ignore color generator on non-git projects

--- go/aliases.zsh
+++ go/aliases.zsh
@@ -3,3 +3,4 @@
 alias go-test-color='go test -v ./... | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/'''
 alias go-mod-graph='gomodblame -o /tmp/graph.mermaid && mmdc -i /tmp/graph.mermaid -o /tmp/graph.svg && open /tmp/graph.svg'
 alias gtf="go-test-fail-fast"
+alias gob="go build"
