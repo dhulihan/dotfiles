2c1a5c22f98737acf33c806f8daa6bfe761ed8a0
Dave Hulihan <dave.hulihan@getweave.com>
2025-04-09 15:58:00 -0600

    keymap tweaks

--- config.nvim/lua/config/keymaps.lua
+++ config.nvim/lua/config/keymaps.lua
@@ -1,10 +1,26 @@
 -- this file contains my personal non-default keymaps. Keeping all keymaps in one file makes lookup easier, and declutters your plugin specs.
 local M = {}
 
+-- transform a string from "foo: bar" to "* `foo` - bar"
+local function transformToInline(input)
+	-- Use string.gsub to replace the pattern
+	local converted = input:gsub("(%w+):%s*(.+)", "* `%1` - %2")
+	return converted
+end
+
 M.setup = function()
-	keys = {
+	leader_keys = {
+		{ "<leader>c", group = "code" },
+
 		-- Leader Favorites
 		{ "<leader>1", "<cmd>set list!<cr>", mode = { "n", "i" } },
+		{
+			"<leader>5",
+			function()
+				require("oil").open_float()
+			end,
+			desc = "Open Oil (float)",
+		},
 		{ "<leader>7", "<cmd>ALEInfo<cr>", mode = { "n", "i" } },
 
 		-- Buffers
@@ -15,13 +31,328 @@ M.setup = function()
 			desc = "delete buffer",
 			mode = { "n", "i" },
 		},
+		{
+			"<leader>bs",
+			"<cmd>Telescope buffers sort_mru=true sort_lastused=true<CR>",
+			desc = "Switch Buffer",
+		},
 
 		{ "<leader>d", group = "debug" },
+		{
+			"<leader>dB",
+			function()
+				require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+			end,
+			desc = "Breakpoint Condition",
+		},
+		{
+			"<leader>db",
+			function()
+				require("dap").toggle_breakpoint()
+			end,
+			desc = "Toggle Breakpoint",
+		},
+		{
+			"<leader>dc",
+			function()
+				require("dap").continue()
+			end,
+			desc = "Continue",
+		},
+		{
+			"<leader>da",
+			function()
+				require("dap").continue({ before = get_args })
+			end,
+			desc = "Run with Args",
+		},
+		{
+			"<leader>dC",
+			function()
+				require("dap").run_to_cursor()
+			end,
+			desc = "Run to Cursor",
+		},
+		{
+			"<leader>dg",
+			function()
+				require("dap").goto_()
+			end,
+			desc = "Go to line (no execute)",
+		},
+		{
+			"<leader>di",
+			function()
+				require("dap").step_into()
+			end,
+			desc = "Step Into",
+		},
+		{
+			"<leader>dj",
+			function()
+				require("dap").down()
+			end,
+			desc = "Down",
+		},
+		{
+			"<leader>dk",
+			function()
+				require("dap").up()
+			end,
+			desc = "Up",
+		},
+		{
+			"<leader>dl",
+			function()
+				require("dap").run_last()
+			end,
+			desc = "Run Last",
+		},
+		{
+			"<leader>do",
+			function()
+				require("dap").step_out()
+			end,
+			desc = "Step Out",
+		},
+		{
+			"<leader>dO",
+			function()
+				require("dap").step_over()
+			end,
+			desc = "Step Over",
+		},
+		{
+			"<leader>dp",
+			function()
+				require("dap").pause()
+			end,
+			desc = "Pause",
+		},
+		{
+			"<leader>dr",
+			function()
+				require("dap").repl.toggle()
+			end,
+			desc = "Toggle REPL",
+		},
+		{
+			"<leader>ds",
+			function()
+				require("dap").session()
+			end,
+			desc = "Session",
+		},
+		{
+			"<leader>dt",
+			function()
+				require("dap").terminate()
+			end,
+			desc = "Terminate",
+		},
+		{
+			"<leader>dw",
+			function()
+				require("dap.ui.widgets").hover()
+			end,
+			desc = "Widgets",
+		},
+		{
+			"<leader>du",
+			function()
+				require("dapui").toggle({})
+			end,
+			desc = "Dap UI",
+		},
+		{
+			"<leader>de",
+			function()
+				require("dapui").eval()
+			end,
+			desc = "Eval",
+			mode = { "n", "v" },
+		},
 
+		-- Errors/Warning/Diagnostics
+		{ "<leader>e", group = "errors" },
+		{
+			"<leader>e]",
+			function()
+				require("trouble").next()
+				require("trouble").jump()
+			end,
+			desc = "Next Trouble Entry",
+			mode = { "n", "v", "i" },
+		},
+		{
+			"<leader>e[",
+			function()
+				require("trouble").prev()
+				require("trouble").jump()
+			end,
+			desc = "Prev Trouble Entry",
+			mode = { "n", "v", "i" },
+		},
+		{
+			"<leader>ed",
+			"<cmd>Trouble diagnostics toggle<CR>",
+			desc = "Project Diagnostics (ALL)",
+		},
+		{
+			"<leader>ee",
+			"<cmd>Trouble diagnostics toggle filter.severity=vim.diagnostic.severity.ERROR<CR>",
+			desc = "Project Diagnostics (ERROR)",
+		},
+		{
+			"<leader>ew",
+			"<cmd>Trouble diagnostics toggle filter.severity=vim.diagnostic.severity.WARN<CR>",
+			desc = "Project Diagnostics (WARN)",
+		},
+		{
+			"<leader>eb",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
+			desc = "Buffer Errors/Warnings - Diagnostics (Trouble)",
+		},
+		{
+			"<leader>es",
+			"<cmd>Trouble symbols toggle focus=false<CR>",
+			desc = "Symbols (Trouble)",
+		},
+		{
+			"<leader>ex",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<CR>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		{
+			"<leader>el",
+			"<cmd>Trouble loclist toggle<CR>",
+			desc = "Location List (Trouble)",
+		},
+		{
+			"<leader>eq",
+			"<cmd>Trouble qflist toggle<CR>",
+			desc = "Quickfix List (Trouble)",
+		},
+
+		-- Find
+		{ "<leader>f", group = "find" },
+		--{ "<leader>/", LazyVim.pick("live_grep"), desc = "Grep (Root Dir)" },
+		--{ "<leader>:", "<cmd>Telescope command_history<CR>", desc = "Command History" },
+		--{ "q:", "<cmd>Telescope command_history<CR>", desc = "Command History" },
+		--{ "<leader><space>", LazyVim.pick("auto"), desc = "Find Files (Root Dir)" },
+		--{ "<C-\\>", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<CR>", desc = "Buffers" }, -- override
+		--{ "<C-p>", "<cmd>Telescope find_files<CR>", desc = "Find Files" }, -- override
+		--{ "<leader>fc", LazyVim.pick.config_files(), desc = "Find Config File" },
+		--{ "<leader>ff", LazyVim.pick("auto"), desc = "Find Files (Root Dir)" },
+		{ "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find Files" },
+		{ "<leader>fb", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<CR>", desc = "Buffers" },
+		--{ "<leader>fF", LazyVim.pick("auto", { root = false }), desc = "Find Files (cwd)" },
+		{ "<leader>fg", "<cmd>Telescope git_files<CR>", desc = "Find Files (git-files)" },
+		{ "<leader>fr", "<cmd>Telescope oldfiles<CR>", desc = "Recently Opened Files" },
+		--{ "<leader>fR", LazyVim.pick("oldfiles", { cwd = vim.uv.cwd() }), desc = "Recent (cwd)" },
+		{ "<leader>ft", "<cmd>Telescope live_grep<CR>", desc = "Find Text (Live Grep)" },
+
+		-- Git
 		{ "<leader>g", group = "git" },
+		{ "<leader>gc", "<cmd>Telescope git_commits<CR>", desc = "Commits" },
+		{ "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "Status" },
+		{ "<leader>gl", "<cmd>Git log -p %<CR>", desc = "Log Patch (Current File)" },
 
-		{ "<leader>q", group = "quickfix" }, -- group
+		-- Pull Requests
+		{
+			"<leader>ps",
+			"<cmd>Octo review start<CR>",
+		},
+		{
+			"<leader>pc<enter>",
+			"<cmd>Octo review comments<CR>",
+			desc = "view pending review comments",
+		},
+		{
+			"<leader>p<enter>",
+			"<cmd>Octo review submit<CR>",
+		},
+		{
+			"<leader>po",
+			":Octo ",
+			desc = "open pr prompt",
+		},
+
+		-- Quickfix
+		{ "<leader>q", group = "quickfix" },
 		{ "<leader>qq", "<cmd>ToggleQuickFix<cr>", desc = "toggle quickfix", mode = { "n", "i" } },
+		{
+			"<leader>qr",
+			function()
+				require("replacer").run()
+			end,
+			desc = "start replacer.nvim to replace text ocurrences in quickfix",
+		},
+		{
+			"<leader>q<enter>",
+			function()
+				require("replacer").run()
+			end,
+			desc = "replace text in quickfix",
+		},
+
+		-- Rewrite/Replace text
+		{ "<leader>r", group = "rewrite" },
+		{
+			"<leader>rl",
+			function()
+				require("vim-dotfiles").replace_bulleted_inline_snippet()
+			end,
+			desc = "Rewrite line in inline code example format",
+		},
+
+		-- Search & Replaces
+		-- Question: what is the difference between find, search, list? Search and Replace
+		{ "<leader>s", group = "search" },
+		{ '<leader>s"', "<cmd>Telescope registers<CR>", desc = "Registers" },
+		{ "<leader>sa", "<cmd>Telescope autocommands<CR>", desc = "Auto Commands" },
+		{ "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<CR>", desc = "Buffer" },
+		{ "<leader>sc", "<cmd>Telescope command_history<CR>", desc = "Command History" },
+		{ "<leader>sC", "<cmd>Telescope commands<CR>", desc = "Commands" },
+		{ "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<CR>", desc = "Document Diagnostics" },
+		{ "<leader>sD", "<cmd>Telescope diagnostics<CR>", desc = "Workspace Diagnostics" },
+		--{ "<leader>sg", "<cmd>Telescope live_grep<CR>", desc = "Live Grep" },
+		--{ "<leader>sg", LazyVim.pick("live_grep"), desc = "Grep (Root Dir)" },
+		--{ "<leader>sG", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
+		{ "<leader>sh", "<cmd>Telescope help_tags<CR>", desc = "Help Pages" },
+		{ "<leader>sH", "<cmd>Telescope highlights<CR>", desc = "Search Highlight Groups" },
+		{ "<leader>sj", "<cmd>Telescope jumplist<CR>", desc = "Jumplist" },
+		{ "<leader>sk", "<cmd>Telescope keymaps<CR>", desc = "Key Maps" },
+		{ "<leader>sl", "<cmd>Telescope loclist<CR>", desc = "Location List" },
+		{ "<leader>sM", "<cmd>Telescope man_pages<CR>", desc = "Man Pages" },
+		{ "<leader>sm", "<cmd>Telescope marks<CR>", desc = "Jump to Mark" },
+		{ "<leader>so", "<cmd>Telescope vim_options<CR>", desc = "Options" },
+		{ "<leader>sR", "<cmd>Telescope resume<CR>", desc = "Resume" },
+		{ "<leader>sq", "<cmd>Telescope quickfix<CR>", desc = "Quickfix List" },
+		--{ "<leader>sw", LazyVim.pick("grep_string", { word_match = "-w" }), desc = "Word (Root Dir)" },
+		--{ "<leader>sW", LazyVim.pick("grep_string", { root = false, word_match = "-w" }), desc = "Word (cwd)" },
+		--{ "<leader>sw", LazyVim.pick("grep_string"), mode = "v", desc = "Selection (Root Dir)" },
+		--{ "<leader>sW", LazyVim.pick("grep_string", { root = false }), mode = "v", desc = "Selection (cwd)" },
+		--{ "<leader>uC", LazyVim.pick("colorscheme", { enable_preview = true }), desc = "Colorscheme with Preview" },
+		--{
+		--"<leader>ss",
+		--function()
+		--require("telescope.builtin").lsp_document_symbols({
+		--symbols = LazyVim.config.get_kind_filter(),
+		--})
+		--end,
+		--desc = "Goto Symbol",
+		--},
+		--{
+		--"<leader>sS",
+		--function()
+		--require("telescope.builtin").lsp_dynamic_workspace_symbols({
+		--symbols = LazyVim.config.get_kind_filter(),
+		--})
+		--end,
+		--desc = "Goto Symbol (Workspace)",
+		--},
+		--
 
 		-- Tests
 		{ "<leader>t", group = "test" },
@@ -215,10 +546,74 @@ M.setup = function()
 			desc = "copy absolute current dir path to clipboard",
 			mode = { "n", "i" },
 		},
+
+		-- Puts
+		{ "<leader>p", group = "put/paste" },
+		{
+			"<leader>pp",
+			'"0p',
+			desc = "paste from yank register 0",
+			mode = { "i", "n", "v" },
+		},
+
+		{ "<leader>a", group = "ai/ml/copilot" },
+		{
+			"<leader>ac",
+			"<cmd>:CopilotChatToggle<CR>",
+			desc = "toggle copilot chat",
+			mode = { "i", "n", "v" },
+		},
+		{
+			"<leader>ar",
+			"<cmd>:CopilotChatReset<CR>",
+			desc = "reset copilot chat",
+			mode = { "i", "n", "v" },
+		},
+		{
+			"<leader>am",
+			"<cmd>:CopilotChatModels<CR>",
+			desc = "copilot chat models",
+			mode = { "i", "n", "v" },
+		},
+		{
+			"<leader>ae",
+			"<cmd>:Copilot enable<CR>",
+			desc = "enable copilot",
+			mode = { "i", "n", "v" },
+		},
+		{
+			"<leader>ar",
+			"<cmd>:CopilotChatReset<CR>",
+			desc = "reset copilot chat",
+			mode = { "i", "n", "v" },
+		},
+	}
+
+	local meta_keys = {
+		-- AI/Copilot
+		{
+			"<M-c>",
+			"<cmd>:CopilotChatToggle<CR>",
+			desc = "toggle copilot chat",
+			mode = { "n", "v" },
+		},
+		{
+			"<M-e>",
+			"<cmd>:Copilot enable<CR>",
+			desc = "enable copilot",
+			mode = { "i", "n", "v" },
+		},
+		{
+			"<M-r>",
+			"<cmd>:CopilotChatReset<CR>",
+			desc = "reset copilot chat",
+			mode = { "i", "n", "v" },
+		},
 	}
 
 	local wk = require("which-key")
-	wk.add(keys)
+	wk.add(leader_keys)
+	wk.add(meta_keys)
 
 	-- non-plugin keymaps
 
@@ -262,9 +657,9 @@ M.setup = function()
 	--vim.keymap.set("n", "<Leader>B", function()
 	--require("dap").set_breakpoint()
 	--end)
-	vim.keymap.set("n", "<Leader>lp", function()
-		require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
-	end)
+	--vim.keymap.set("n", "<Leader>lp", function()
+	--require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+	--end)
 	vim.keymap.set({ "n", "v" }, "<Leader>dh", function()
 		require("dap.ui.widgets").hover()
 	end)
--- config.nvim/lua/plugins/plugins.lua
+++ config.nvim/lua/plugins/plugins.lua
@@ -36,22 +36,6 @@ return {
 				end,
 			},
 		},
-		keys = {
-			{
-				"<F1>",
-				function()
-					require("copilot.suggestion").toggle_auto_trigger()
-				end,
-			},
-			{
-				"<F1>",
-				function()
-					--vim.api.nvim_err_writeln(string.format("%s", os.time()))
-					require("copilot.suggestion").toggle_auto_trigger()
-				end,
-				mode = "i",
-			},
-		},
 	},
 	{
 		"jonahgoldwastaken/copilot-status.nvim",
@@ -81,22 +65,12 @@ return {
 			--debug = true, -- Enable debugging
 			-- See Configuration section for rest
 			model = "claude-3.5-sonnet",
+			window = {
+				layout = "float",
+				border = "rounded",
+			},
 		},
 		-- See Commands section for default commands if you want to lazy load on them
-		keys = {
-			{
-				"<M-c>",
-				"<cmd>:CopilotChat<CR>",
-				desc = "open copilot chat",
-				mode = { "n", "v" },
-			},
-			{
-				"<M-e>",
-				"<cmd>:Copilot enable<CR>",
-				desc = "enable copilot",
-				mode = { "i", "n", "v" },
-			},
-		},
 	},
 	{ "nvim-lua/plenary.nvim" },
 	{
@@ -114,7 +88,7 @@ return {
 					dynamic_preview_title = true,
 					layout_strategy = "vertical",
 					layout_config = {
-						vertical = { width = 0.9, height = 0.9, preview_height = 0.6, preview_cutoff = 0 },
+						vertical = { width = 0.9, height = 0.9, preview_height = 0.7, preview_cutoff = 0 },
 					},
 					--path_display = { "smart", shorten = { len = 5 } },
 					path_display = { "absolute" },
@@ -137,79 +111,6 @@ return {
 				},
 			})
 		end,
-		keys = {
-			{
-				"<leader>,",
-				"<cmd>Telescope buffers sort_mru=true sort_lastused=true<CR>",
-				desc = "Switch Buffer",
-			},
-			--{ "<leader>/", LazyVim.pick("live_grep"), desc = "Grep (Root Dir)" },
-			--{ "<leader>:", "<cmd>Telescope command_history<CR>", desc = "Command History" },
-			--{ "q:", "<cmd>Telescope command_history<CR>", desc = "Command History" },
-			--{ "<leader><space>", LazyVim.pick("auto"), desc = "Find Files (Root Dir)" },
-
-			-- list
-			--{ "<C-\\>", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<CR>", desc = "Buffers" }, -- override
-			--{ "<C-p>", "<cmd>Telescope find_files<CR>", desc = "Find Files" }, -- override
-			--{ "<leader>fc", LazyVim.pick.config_files(), desc = "Find Config File" },
-			--{ "<leader>ff", LazyVim.pick("auto"), desc = "Find Files (Root Dir)" },
-			{ "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find Files" },
-			{ "<leader>fb", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<CR>", desc = "Buffers" },
-			--{ "<leader>fF", LazyVim.pick("auto", { root = false }), desc = "Find Files (cwd)" },
-			{ "<leader>fg", "<cmd>Telescope git_files<CR>", desc = "Find Files (git-files)" },
-			{ "<leader>fr", "<cmd>Telescope oldfiles<CR>", desc = "Recently Opened Files" },
-			--{ "<leader>fR", LazyVim.pick("oldfiles", { cwd = vim.uv.cwd() }), desc = "Recent (cwd)" },
-			{ "<leader>ft", "<cmd>Telescope live_grep<CR>", desc = "Find Text (Live Grep)" },
-
-			-- git
-			{ "<leader>gc", "<cmd>Telescope git_commits<CR>", desc = "Commits" },
-			{ "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "Status" },
-
-			-- search
-			{ '<leader>s"', "<cmd>Telescope registers<CR>", desc = "Registers" },
-			{ "<leader>sa", "<cmd>Telescope autocommands<CR>", desc = "Auto Commands" },
-			{ "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<CR>", desc = "Buffer" },
-			{ "<leader>sc", "<cmd>Telescope command_history<CR>", desc = "Command History" },
-			{ "<leader>sC", "<cmd>Telescope commands<CR>", desc = "Commands" },
-			{ "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<CR>", desc = "Document Diagnostics" },
-			{ "<leader>sD", "<cmd>Telescope diagnostics<CR>", desc = "Workspace Diagnostics" },
-			{ "<leader>sg", "<cmd>Telescope live_grep<CR>", desc = "Live Grep" },
-			--{ "<leader>sg", LazyVim.pick("live_grep"), desc = "Grep (Root Dir)" },
-			--{ "<leader>sG", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
-			{ "<leader>sh", "<cmd>Telescope help_tags<CR>", desc = "Help Pages" },
-			{ "<leader>sH", "<cmd>Telescope highlights<CR>", desc = "Search Highlight Groups" },
-			{ "<leader>sj", "<cmd>Telescope jumplist<CR>", desc = "Jumplist" },
-			{ "<leader>sk", "<cmd>Telescope keymaps<CR>", desc = "Key Maps" },
-			{ "<leader>sl", "<cmd>Telescope loclist<CR>", desc = "Location List" },
-			{ "<leader>sM", "<cmd>Telescope man_pages<CR>", desc = "Man Pages" },
-			{ "<leader>sm", "<cmd>Telescope marks<CR>", desc = "Jump to Mark" },
-			{ "<leader>so", "<cmd>Telescope vim_options<CR>", desc = "Options" },
-			{ "<leader>sR", "<cmd>Telescope resume<CR>", desc = "Resume" },
-			{ "<leader>sq", "<cmd>Telescope quickfix<CR>", desc = "Quickfix List" },
-			--{ "<leader>sw", LazyVim.pick("grep_string", { word_match = "-w" }), desc = "Word (Root Dir)" },
-			--{ "<leader>sW", LazyVim.pick("grep_string", { root = false, word_match = "-w" }), desc = "Word (cwd)" },
-			--{ "<leader>sw", LazyVim.pick("grep_string"), mode = "v", desc = "Selection (Root Dir)" },
-			--{ "<leader>sW", LazyVim.pick("grep_string", { root = false }), mode = "v", desc = "Selection (cwd)" },
-			--{ "<leader>uC", LazyVim.pick("colorscheme", { enable_preview = true }), desc = "Colorscheme with Preview" },
-			--{
-			--"<leader>ss",
-			--function()
-			--require("telescope.builtin").lsp_document_symbols({
-			--symbols = LazyVim.config.get_kind_filter(),
-			--})
-			--end,
-			--desc = "Goto Symbol",
-			--},
-			--{
-			--"<leader>sS",
-			--function()
-			--require("telescope.builtin").lsp_dynamic_workspace_symbols({
-			--symbols = LazyVim.config.get_kind_filter(),
-			--})
-			--end,
-			--desc = "Goto Symbol (Workspace)",
-			--},
-		},
 	},
 	{
 		"nvim-treesitter/nvim-treesitter",
@@ -239,38 +140,6 @@ return {
 		opts = {
 			mode = "quickfix", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
 		},
-		keys = {
-			{
-				"<leader>ep",
-				"<cmd>Trouble diagnostics toggle filter.severity=vim.diagnostic.severity.ERROR<CR>",
-				desc = "Project Errors - Diagnostics (Trouble)",
-			},
-			{
-				"<leader>eb",
-				"<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
-				desc = "Buffer Errors/Warnings - Diagnostics (Trouble)",
-			},
-			{
-				"<leader>es",
-				"<cmd>Trouble symbols toggle focus=false<CR>",
-				desc = "Symbols (Trouble)",
-			},
-			{
-				"<leader>ex",
-				"<cmd>Trouble lsp toggle focus=false win.position=right<CR>",
-				desc = "LSP Definitions / references / ... (Trouble)",
-			},
-			{
-				"<leader>el",
-				"<cmd>Trouble loclist toggle<CR>",
-				desc = "Location List (Trouble)",
-			},
-			{
-				"<leader>eq",
-				"<cmd>Trouble qflist toggle<CR>",
-				desc = "Quickfix List (Trouble)",
-			},
-		},
 	}, -- error helpers
 	--{ "benmills/vimux" }, -- not using recently
 
@@ -290,7 +159,7 @@ return {
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-emoji",
 			"hrsh7th/cmp-cmdline",
-			"uga-rosa/cmp-dictionary",
+			--"uga-rosa/cmp-dictionary",
 			--"quangnguyen30192/cmp-nvim-ultisnips",
 		},
 
@@ -384,6 +253,12 @@ return {
 					end, { "i", "s" }),
 				},
 				-- })
+				formatting = {
+					format = function(entry, vim_item)
+						vim_item.menu = entry.source.name
+						return vim_item
+					end,
+				},
 				sources = {
 					{ name = "luasnip" },
 					{
@@ -399,11 +274,12 @@ return {
 							end,
 						},
 					},
-					{
-						name = "dictionary",
-						paths = { "/usr/share/dict/words" },
-						keyword_length = 2,
-					},
+					-- 2025-04-09 disabling, too noisy
+					--{
+					--name = "dictionary",
+					--paths = { "/usr/share/dict/words" },
+					--keyword_length = 2,
+					--},
 					{ name = "emoji" },
 					{ name = "path" },
 					{
@@ -432,7 +308,11 @@ return {
 		opts = {
 			keymaps = {
 				["<Leader>5"] = { "actions.close", mode = "n" },
-				["<C-c>"] = { "", mode = "n" },
+				["<C-c>"] = { "actions.close", mode = "n" },
+			},
+			view_options = {
+				-- Show files and directories that start with "."
+				show_hidden = true,
 			},
 		},
 		-- Optional dependencies
@@ -554,26 +434,6 @@ return {
 				mappings_disable_default = true,
 			})
 		end,
-		keys = {
-			{
-				"<leader>ps",
-				"<cmd>Octo review start<CR>",
-			},
-			{
-				"<leader>pc<enter>",
-				"<cmd>Octo review comments<CR>",
-				desc = "view pending review comments",
-			},
-			{
-				"<leader>p<enter>",
-				"<cmd>Octo review submit<CR>",
-			},
-			{
-				"<leader>po",
-				":Octo ",
-				desc = "open pr prompt",
-			},
-		},
 	},
 	{
 		"rmagatti/auto-session",
@@ -718,6 +578,7 @@ return {
 	-- Testing
 	{
 		"quolpr/quicktest.nvim",
+		--enabled = false -- 2025-02-26 trouble with buffer numbers on session restore
 		config = function()
 			local qt = require("quicktest")
 
@@ -880,131 +741,9 @@ return {
 
 			require("neotest").setup(opts)
 		end,
-		keys = {},
 	},
 	{
 		"mfussenegger/nvim-dap",
-		keys = {
-			{
-				"<leader>dB",
-				function()
-					require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
-				end,
-				desc = "Breakpoint Condition",
-			},
-			{
-				"<leader>db",
-				function()
-					require("dap").toggle_breakpoint()
-				end,
-				desc = "Toggle Breakpoint",
-			},
-			{
-				"<leader>dc",
-				function()
-					require("dap").continue()
-				end,
-				desc = "Continue",
-			},
-			{
-				"<leader>da",
-				function()
-					require("dap").continue({ before = get_args })
-				end,
-				desc = "Run with Args",
-			},
-			{
-				"<leader>dC",
-				function()
-					require("dap").run_to_cursor()
-				end,
-				desc = "Run to Cursor",
-			},
-			{
-				"<leader>dg",
-				function()
-					require("dap").goto_()
-				end,
-				desc = "Go to line (no execute)",
-			},
-			{
-				"<leader>di",
-				function()
-					require("dap").step_into()
-				end,
-				desc = "Step Into",
-			},
-			{
-				"<leader>dj",
-				function()
-					require("dap").down()
-				end,
-				desc = "Down",
-			},
-			{
-				"<leader>dk",
-				function()
-					require("dap").up()
-				end,
-				desc = "Up",
-			},
-			{
-				"<leader>dl",
-				function()
-					require("dap").run_last()
-				end,
-				desc = "Run Last",
-			},
-			{
-				"<leader>do",
-				function()
-					require("dap").step_out()
-				end,
-				desc = "Step Out",
-			},
-			{
-				"<leader>dO",
-				function()
-					require("dap").step_over()
-				end,
-				desc = "Step Over",
-			},
-			{
-				"<leader>dp",
-				function()
-					require("dap").pause()
-				end,
-				desc = "Pause",
-			},
-			{
-				"<leader>dr",
-				function()
-					require("dap").repl.toggle()
-				end,
-				desc = "Toggle REPL",
-			},
-			{
-				"<leader>ds",
-				function()
-					require("dap").session()
-				end,
-				desc = "Session",
-			},
-			{
-				"<leader>dt",
-				function()
-					require("dap").terminate()
-				end,
-				desc = "Terminate",
-			},
-			{
-				"<leader>dw",
-				function()
-					require("dap.ui.widgets").hover()
-				end,
-				desc = "Widgets",
-			},
-		},
 		config = function()
 			local dap = require("dap")
 			--require("dap").set_log_level("DEBUG")
@@ -1021,23 +760,6 @@ return {
 	},
 	{
 		"rcarriga/nvim-dap-ui",
-		keys = {
-			{
-				"<leader>du",
-				function()
-					require("dapui").toggle({})
-				end,
-				desc = "Dap UI",
-			},
-			{
-				"<leader>de",
-				function()
-					require("dapui").eval()
-				end,
-				desc = "Eval",
-				mode = { "n", "v" },
-			},
-		},
 		opts = {},
 		config = function(_, opts)
 			-- setup dap config by VsCode launch.json file
@@ -1073,22 +795,6 @@ return {
 	{
 		"gabrielpoca/replacer.nvim", -- replace using quickfix!
 		--opts = {rename_files = false},
-		keys = {
-			{
-				"<leader>h",
-				function()
-					require("replacer").run()
-				end,
-				desc = "run replacer.nvim",
-			},
-			{
-				"<leader>qr",
-				function()
-					require("replacer").run()
-				end,
-				desc = "replace text in quickfix",
-			},
-		},
 	},
 	{ "mileszs/ack.vim" },
 	{ "jremmen/vim-ripgrep" },
@@ -1482,9 +1188,6 @@ return {
 			--vim.g.minimap_auto_start_win_enter = 1
 			vim.g.minimap_highlight_search = 1 -- highlight search patterns
 		end,
-		--keys = {
-		--{ "<F3>", "<cmd>MinimapToggle<CR>", mode = { "n", "i" } },
-		--},
 	}, -- requires code-minimap to be installed separately
 	{ "ryanoasis/vim-devicons" },
 
